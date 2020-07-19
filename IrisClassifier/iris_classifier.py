a991df11c5ab3cf6235090908d85767002ddf2a6
root <root@LawUX433.localdomain>
2020-07-19 23:50:27 +0800

    first commit

---
+++ IrisClassifier/iris_classifier.py
@@ -0,0 +1,12 @@
+from bentoml import env, artifacts, api, BentoService
+from bentoml.adapters import DataframeInput
+from bentoml.artifact import SklearnModelArtifact
+
+@env(auto_pip_dependencies=True)
+@artifacts([SklearnModelArtifact('model')])
+class IrisClassifier(BentoService):
+
+    @api(input=DataframeInput())
+    def predict(self, df):
+        # Optional pre-processing, post-processing code goes here
+        return self.artifacts.model.predict(df)
